d1126a21f402adf05f3110a0715ebaeebb5ff2fd
AlexSawranskiy <alexsawranskiy989@gmail.com>
2023-07-13 19:33:41 +0300

    add collection with information

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import painting from './painting.json';
+import { GalleryItem } from './components/GalleryItem/Gallery';
+
+const item = painting[0];
+
+export const App = () => {
+  return (
+     <>
+      <GalleryItem 
+      itemUrl={item.url}
+      title={item.title}
+      author={item.author.url}
+      price={item.price}
+      quantity={item.quantity}
+      tag={item.author.tag}/>
+     </>
+    )
+}
