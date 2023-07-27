6f153dc09618706d428e29fcd0b69897b49f1681
AlexSawranskiy <alexsawranskiy989@gmail.com>
2023-07-27 19:33:10 +0300

    add color components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 // import painting from './painting.json';
 // import { GalleryList } from './components/GalleryList';
-import { Recipe } from "./components/Recipe"
-import recipe from "./recipies.json"
+// import { GalleryList } from "./components/GalleryList"
+// import { Recipe } from "./components/Recipe"
+// import recipe from "./recipies.json"
+// import PropTypes from 'prop-types';
+import { ColorPicker } from './components/ColorPicker/ColorPicker';
+import { colorPickerOptions } from './components/ColorPicker/colorpickerData';
+import { Container } from './components/Container/Container';
 
 export const App = () => {
   return (
      <>
-       {/* <GalleryList items={painting}/> */}
+       <Container>
+          <ColorPicker options = {colorPickerOptions}/>
+       </Container>
+       {/* <GalleryList items={painting}/>
        {recipe.map(({name, image, time, servings, calories}) => {
         return <Recipe
           key = {image}
@@ -16,7 +24,7 @@ export const App = () => {
           servings = {servings}
           calories = {calories}
         />
-       })}
+       })} */}
      </>
     )
 }
--- src/components/GalleryItem/Gallery.jsx
+++ src/components/GalleryItem/Gallery.jsx
@@ -1,3 +1,5 @@
+import '../GalleryItem/Gallery.css'
+
 export const GalleryItem = ({
     itemUrl,
     title,
@@ -8,7 +10,7 @@ export const GalleryItem = ({
     return(
         <>
          <img src={itemUrl} alt="" width="480" />
-         <h2>{title}</h2>
+         <h2 className = 'title'>{title}</h2>
          <p>Автор: <a href={author}>(tag)</a></p>
          <p>Ціна: {price} грн</p>
          <p>Доступність: {quantity} закінчується чи є в наявності</p>
@@ -16,3 +18,12 @@ export const GalleryItem = ({
         </>
     )
 }
+
+// GalleryItem.propTypes = {
+//     itemUrl: PropTypes.string.isRequired,
+//     title: PropTypes.string.isRequired,
+//     author: PropTypes.string.isRequired,
+//     price: PropTypes.number.isRequired,
+//     quantity: PropTypes.number.isRequired,
+//     tag: PropTypes.string.isRequired,
+// }
